db8ba276659efb3a302b7d50e1766e8166cd36c8
Coding is Fun <jack_zhao98@outlook.com>
2018-12-09 11:02:21 -0800

    Update operator.hpp
    Comments added

--- Calculator/Header Files/operator.hpp
+++ Calculator/Header Files/operator.hpp
@@ -19,8 +19,21 @@ public:
     optr(char _op = char());
     optr(std::string op = std::string());
     double do_math(const double& Unary);
+    /*
+    *. Overloaded function.
+    *   Do Unary operation if there's only one argument.
+    */
     double do_math(const double& LHS, const double& RHS);
+    /*
+    *. Overloaded function.
+    *   Do Binary operation if there are two arguments.
+    */
     double Tan(const double& var);
+    /*
+    *   I attempted to write tangent function with Newton's Method to solve 
+    *     discontinuity issue in graphing.
+    *   Does not work.
+    */
     std::ostream& print(std::ostream& out)const;
 };
 
