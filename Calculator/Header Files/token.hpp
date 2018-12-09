d0de511ee7d5edcf16bc02f17fa213de73093e05
Coding is Fun <jack_zhao98@outlook.com>
2018-12-09 11:08:39 -0800

    Update token.hpp

--- Calculator/Header Files/token.hpp
+++ Calculator/Header Files/token.hpp
@@ -9,21 +9,43 @@
 #ifndef token_hpp
 #define token_hpp
 #include <iostream>
+
+// Class name: token
+// Parent class of oprand and optr
+
 class token {
 private:
+    // Token type.
     char _type;
 
 public:
     token(char c = 'e'): _type(c) {}
     
-    
+    // Accessor.
     char type() {return _type;}
+    // Modifier.
+    // setup token type.
     void set_type(char c) {_type = c;}
-    virtual double get_val(){return 0;}
-    virtual double do_math(const double& LHS, const double& RHS){return 0;}
-    virtual double do_math(const double& Unary){return 0;}
-    virtual std::ostream& print(std::ostream& out)const;
     
+    // Virtual functions.
+    virtual double get_val(){return 0;}
+    /*
+    *   This function points to oprand type funcitons and
+    *   return the oprand value.
+    */
+    virtual double do_math(const double& LHS, const double& RHS){return 0;}
+    /*
+    *   This function points to optr type funcitons and
+    *   do binary operations.
+    */
+    virtual double do_math(const double& Unary){return 0;}
+    /*
+    *   This function points to optr type funcitons and
+    *   do unary operations.
+    */
+    
+    // IOStream, for dubug purposes.
+    virtual std::ostream& print(std::ostream& out)const;
     friend std::ostream& operator << (std::ostream& out, const token* _t);
 };
 
