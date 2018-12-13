21ff7b4b3cfa4b1b61a7c8621c9b18af688ca507
Coding is Fun <jack_zhao98@outlook.com>
2018-12-12 18:10:38 -0800

    Comments updated
    Comments updated

--- VertexRect.hpp
+++ VertexRect.hpp
@@ -11,19 +11,25 @@
 #include "UIKit Ripoff/CoreGraphics/CGPoint.hpp"
 class VertexRect {
 private:
-    sf::RectangleShape rect;
-    int stroke;
-    CGPoint start;
-    CGPoint end;
+        
+    sf::RectangleShape rect;    // SFML object, draw this.
+    int stroke;                 // The desired width of rectangle line.
+    CGPoint start;              // Starting position.
+    CGPoint end;                // Ending position.
     
     
 public:
+    
+    // CTORS
     VertexRect(): stroke(3) {}
     
+    // Constructor with vertexes
     VertexRect(const CGPoint& _start, const CGPoint& _end): stroke(3), start(_start), end(_end) {
         
     }
     
+    // Initialize the sfml rectangle with preset params.    
+    // Run the init process and returns the sf::RectangleShape
     void initVertexRect() {
         rect.setOrigin(0, 0.5 * stroke);
         rect.setPosition(sf::Vector2f(start.x, start.y));
@@ -33,46 +39,53 @@ public:
         rect.setRotation(getAngle());
     }
     
+
+    // Accessor
     const sf::RectangleShape getRect() {
         initVertexRect();
         return rect;
     }
-    /*
-    *   Run the init process and returns the sf::RectangleShape
-    */
+
+ 
+    // Modifier
     
-    double getAngle() {
-        double tan = atan(end.y - start.y);
-        return tan*180/3.141592653589;
-    }
     /*
     *   Calculate rotation angle using tangent
     *   We have the Y-difference from two points, and the x-difference is one pixel.
     */
-    
+    double getAngle() {
+        double tan = atan(end.y - start.y);
+        return tan*180/3.141592653589;
+    }
+    // Calculate the length of rect
     double getLength() {
         double dx2 = pow((end.x - start.x),2);
         double dy2 = pow((end.y - start.y),2);
         return (pow(dx2 + dy2, 0.5));
     }
-    
+    // Setup init position of rect
     void setPosition(sf::Vector2f position) {
         rect.setPosition(position);
-    }
-    void setStroke(int _stroke) {
-        stroke = _stroke;
-    }
-    void setStart(CGPoint _start) {
-        start = _start;
-    }
-    void setEnd(CGPoint _end) {
-        end = _end;
     }
     void setColor(const sf::Color& color) {
         rect.setOutlineColor(color);
         rect.setFillColor(color);
     }
     
+    // Param modifications 
+    // Setup stroke of the rect
+    void setStroke(int _stroke) {
+        stroke = _stroke;
+    }
+    // Setup starting position
+    void setStart(CGPoint _start) {
+        start = _start;
+    }
+    // Setup ending position
+    void setEnd(CGPoint _end) {
+        end = _end;
+    }
+
 };
 
 #endif /* VertexRect_hpp */
